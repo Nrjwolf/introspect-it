693ec6ff99328e6a3d2f5cc9ab92e80337547f11
David Shek <ditreyw@gmail.com>
2023-02-09 21:45:38 +0000

    feat: added export and yargs

--- bin/index.ts
+++ bin/index.ts
@@ -1,27 +1,89 @@
 #! /usr/bin/env node
-import { inferSchema, inferTable } from '../src'
+import yargs = require("yargs/yargs");
+import { hideBin } from "yargs/helpers";
+import { exportTable, inferSchema, inferTable } from "../src";
 
-const [...args] = process.argv
+const main = (): void => {
+  yargs(hideBin(process.argv))
+    .options({
+      connection: { type: "string", default: process.env.DATABASE_URL },
+      toCamelCase: { type: "boolean", default: process.env.CAMEL_CASE === "true" || false },
+      useQuotes: { type: "boolean", default: process.env.USE_QUOTES === "true" || false }
+    })
+    .command(
+      "export [table] [primaryKey]",
+      "export table",
+      yargs => {
+        return yargs
+          .positional("table", {
+            describe: "table to export",
+            type: "string"
+          })
+          .positional("primaryKey", {
+            describe: "primary key of table",
+            default: "id",
+            type: "string"
+          });
+      },
+      argv => {
+        const { table, connection, primaryKey } = argv;
 
-async function main(): Promise<string> {
-  const db = args[2] || process.env.DATABASE_URL || ''
-  const table = args[3] || process.env.TABLE_NAME
-  const toCamelCase = process.env.CAMEL_CASE === 'true' || false
-  const useQuotes = process.env.USE_QUOTES === 'true' || false
+        if (!connection || !table) {
+          throw new Error(`Table and db are required`);
+        }
 
-  if (table) {
-    return inferTable(db, table, toCamelCase, useQuotes)
-  }
+        exportTable(connection, table, primaryKey)
+          .then(code => {
+            console.log(code);
+            process.exit();
+          })
+          .catch(err => {
+            console.error(err);
+            process.exit(1);
+          });
+      }
+    )
+    .command(
+      "infer [table]",
+      "infer table",
+      yargs => {
+        return yargs.positional("table", {
+          describe: "table to export",
+          type: "string"
+        });
+      },
+      argv => {
+        const { table, connection, toCamelCase, useQuotes } = argv;
 
-  return inferSchema(db, toCamelCase, useQuotes)
-}
+        if (!connection) {
+          throw new Error(`Table and pgConnection are required`);
+        }
 
-main()
-  .then(code => {
-    console.log(code)
-    process.exit()
-  })
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
+        if (table) {
+          inferTable(connection, table, toCamelCase, useQuotes)
+            .then(code => {
+              console.log(code);
+              process.exit();
+            })
+            .catch(err => {
+              console.error(err);
+              process.exit(1);
+            });
+          return;
+        }
+
+        inferSchema(connection, toCamelCase, useQuotes)
+          .then(code => {
+            console.log(code);
+            process.exit();
+          })
+          .catch(err => {
+            console.error(err);
+            process.exit(1);
+          });
+      }
+    )
+    .parse();
+};
+
+main();
